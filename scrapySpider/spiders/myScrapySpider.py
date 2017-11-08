205126105dd93d1131ec0856eba55acca2c55150
ja0ck5 <975117619@qq.com>
2017-11-08 15:55:21 +0800

    scrapy

--- scrapySpider/spiders/myScrapySpider.py
+++ scrapySpider/spiders/myScrapySpider.py
@@ -2,6 +2,8 @@
 
 import scrapy
 
+from scrapySpider.items import PeopleItem
+
 
 class MyScrapySpider(scrapy.spiders.Spider):
     name = "MyScrapySpider"
@@ -13,5 +15,24 @@ class MyScrapySpider(scrapy.spiders.Spider):
     ]
 
     def parse(self, response):
-        file_name = "D:/teacher.html"
-        open(file_name, 'w').write(response.body)
+        items = []
+        # file_name = "D:/teacher.html"
+        # open(file_name, 'w').write(response.body)
+        for site in response.xpath('//div[@class="li_txt"]'):
+            item = PeopleItem()
+
+            teacher_name = site.xpath('h3/text()').extract()
+            teacher_level = site.xpath('h4/text()').extract()
+            teacher_info = site.xpath('p/text()').extract()
+            print teacher_name
+            print teacher_level
+            print teacher_info
+            print '====================='
+
+            item['name'] = teacher_name[0]
+            item['level'] = teacher_level[0]
+            item['info'] = teacher_info[0]
+
+            items.append(item)
+
+        return items
